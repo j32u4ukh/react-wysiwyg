30a2cd9fa2d23e38ea9283f7e7950f76a6de6f1c
j32u4ukh <j32u4ukh@gmail.com>
2024-03-10 22:47:34 +0800

    feat: 將原始範例頁面加入

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import "./styles.css";
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,6 +8,7 @@ import NotFound from "./pages/NotFound";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import Product from "./pages/Product";
+import VitePage from "./pages/VitePage";
 
 export default function App() {
   return (
@@ -45,6 +44,7 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="page1" element={<Page1 />} />
           <Route path="page2" element={<Page2 />} />
+          <Route path="vite" element={<VitePage />} />
 
           <Route path="product/:id" element={<Product/>} />
 
@@ -57,33 +57,3 @@ export default function App() {
   );
 }
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
