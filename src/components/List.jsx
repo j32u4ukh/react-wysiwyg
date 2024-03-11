10f3132648bb935c12864b459b69ac6f10ddb490
j32u4ukh <j32u4ukh@gmail.com>
2024-03-11 23:16:03 +0800

    feat: 遍歷生成列表

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,7 @@
 export default function List(props) {
-  const { name, price } = props;
   return (
     <li>
-      <span>{name}</span>
-      <span>${price}</span>
+      {props.index + 1}. {props.name} ${props.price}
     </li>
   );
 }
--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,13 +1,32 @@
 import List from "../components/List";
 
 export default function Restaurant(){
+    const menu = [
+        { name: "蘆筍沙拉", price: 100 },
+        { name: "辣炒空心菜", price: 120 },
+        { name: "雞蛋豆腐", price: 150 },
+        { name: "鳳梨蝦球", price: 300 },
+        { name: "糖醋雞丁", price: 200 },
+        { name: "砂鍋魚頭", price: 500 },
+        { name: "竹筍炒肉絲", price: 150 },
+        { name: "梨山高麗菜", price: 120 },
+        { name: "五更腸旺", price: 250 },
+        { name: "客家小炒", price: 250 },
+        { name: "三杯杏鮑菇", price: 180 }
+    ];
+
     return (
         <div>
           <h1>React 熱炒店</h1>
           <ul>
-            <List name="蘆筍沙拉" price={100} />
-            <List name="辣炒空心菜" price={120} />
-            <List name="雞蛋豆腐" price={150} />
+            {menu.map((item, index) => {
+                return (<List
+                    key={item.name}
+                    name={item.name}
+                    price={item.price}
+                    index={index}
+                ></List>)
+            })}
           </ul>
         </div>
       );
