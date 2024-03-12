bc4d9466b3f5321fc5482bae2f75daf9fce88eed
j32u4ukh <j32u4ukh@gmail.com>
2024-03-12 23:28:34 +0800

    feat: 圖片懶加載(Lazy load)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,25 @@
 import Counter from "./Counter";
 import Styles from './List.module.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function List(props) {
+  const { name, price, isLimited, image } = props.item;
   return (
     <li className={Styles.item}>
-      <div className={Styles.name}>{props.name}</div>
-      <span>${props.price}</span>
+      <LazyLoadImage
+        src={image}
+        alt=""
+        style={{
+          width: "80px",
+          height: "80px",
+          backgroundColor: "#ffe691"
+        }}
+      />
+      <div className={Styles.name}>
+        <span>{name}</span>
+        {isLimited && <span className={Styles.limited}>(限量供應)</span>}
+      </div>
+      <span>${price}</span>
       <Counter />
     </li>
   );
--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -4,17 +4,78 @@ import Styles from '../css/Restaurant.module.css'
 
 export default function Restaurant(){
     const menu = [
-        { name: "蘆筍沙拉", price: 100 },
-        { name: "辣炒空心菜", price: 120 },
-        { name: "雞蛋豆腐", price: 150 },
-        { name: "鳳梨蝦球", price: 300 },
-        { name: "糖醋雞丁", price: 200 },
-        { name: "砂鍋魚頭", price: 500 },
-        { name: "竹筍炒肉絲", price: 150 },
-        { name: "梨山高麗菜", price: 120 },
-        { name: "五更腸旺", price: 250 },
-        { name: "客家小炒", price: 250 },
-        { name: "三杯杏鮑菇", price: 180 }
+      {
+        name: "蘆筍沙拉",
+        price: 100,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=1"
+      },
+      {
+        name: "辣炒空心菜",
+        price: 120,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=2"
+      },
+      {
+        name: "雞蛋豆腐",
+        price: 150,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=3"
+      },
+      {
+        name: "鳳梨蝦球",
+        price: 300,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=4"
+      },
+      {
+        name: "糖醋雞丁",
+        price: 200,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=5"
+      },
+      {
+        name: "砂鍋魚頭",
+        price: 500,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=6"
+      },
+      {
+        name: "竹筍炒肉絲",
+        price: 150,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=7"
+      },
+      {
+        name: "梨山高麗菜",
+        price: 120,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=8"
+      },
+      {
+        name: "五更腸旺",
+        price: 250,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=9"
+      },
+      {
+        name: "客家小炒",
+        price: 250,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=10"
+      },
+      {
+        name: "三杯杏鮑菇",
+        price: 180,
+        isLimited: false,
+        image: "https://picsum.photos/80/80?random=11"
+      },
+      {
+        name: "黃金開口笑",
+        price: 250,
+        isLimited: true,
+        image: "https://picsum.photos/80/80?random=12"
+      }
     ];
 
     return (
@@ -25,8 +86,7 @@ export default function Restaurant(){
             {menu.map((item, index) => {
                 return (<List
                     key={item.name}
-                    name={item.name}
-                    price={item.price}
+                    item={item}
                     index={index}
                 ></List>)
             })}
